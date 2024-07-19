bb1f4ea0661b6302156a9653113509e98859c703
AliAliyev2004 <eli.eliyev.200424@gmail.com>
2024-07-19 15:47:22 +0400

    IlK COMMIT

---
+++ LINQ/Debtor.cs
@@ -0,0 +1,30 @@
+﻿namespace LINQ;
+
+using System;
+
+
+
+    public class Debtor
+    {
+        public string FullName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public int Debt { get; set; }
+
+        public Debtor(string fullName, DateTime birthDate, string phoneNumber, string email, string address, int debt)
+        {
+            FullName = fullName;
+            BirthDate = birthDate;
+            PhoneNumber = phoneNumber;
+            Email = email;
+            Address = address;
+            Debt = debt;
+        }
+
+        public override string ToString()
+        {
+            return $"{FullName}, {BirthDate.ToShortDateString()}, {PhoneNumber}, {Email}, {Address}, {Debt}";
+        }
+    }
